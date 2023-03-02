0011064d1cb3fe4979720d87168f1efb3a30a25f
hirotoe0112 <hirotoe0112@gmail.com>
2023-03-02 20:11:20 +0900

    initial commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// 問題名=クラス名、実行用メソッド=Runで統一する
+var instance = new TwoSum();
+var result = instance.Run(new int[]{3,1,1,1,2,1,1,4}, 6);
+Console.WriteLine(string.Join(",", result));
