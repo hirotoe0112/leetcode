2ca7ed7d6029108b26ccdfbfd7299af12aea7fa2
hirotoe0112 <hirotoe0112@gmail.com>
2023-08-08 09:02:36 +0900

    merge two sorted lists 途中

--- Program.cs
+++ Program.cs
@@ -2,8 +2,9 @@
 // Console.WriteLine("Hello, World!");
 
 // 問題名=クラス名、実行用メソッド=Runで統一する
-var instance = new ValidParentheses();
-var result = instance.Run("{[");
+var instance = new MergeTwoSortedLists();
+//var result = instance.Run(new ListNode(1, new ListNode(2, new ListNode(4))), new ListNode(1, new ListNode(3, new ListNode(4))));
+var result = instance.Run(new ListNode(1, new ListNode(2, new ListNode(4))), new ListNode(1, new ListNode(3, new ListNode(4))));
 if(result.GetType() == typeof(Array)){
     // 答えが配列用
     Console.WriteLine(string.Join(",", result));
---
+++ Solutions/MergeTwoSortedLists.cs
@@ -0,0 +1,23 @@
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+public class MergeTwoSortedLists
+{
+    public ListNode Run(ListNode list1, ListNode list2)
+    {
+
+
+
+
+
+
+        return list1;
+    }
+}
