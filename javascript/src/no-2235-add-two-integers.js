aca228e35aaa90dea1213492f2bfdce3011e8137
hirotoe0112 <hirotoe0112@gmail.com>
2024-09-11 16:03:47 +0900

    2235/javascript

---
+++ javascript/src/no-2235-add-two-integers.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+var sum = function(num1, num2) {
+  return num1 + num2;
+};
+
+// Parse the command line arguments
+// NOTE: In JavaScript, the first two arguments are always the path to Node.js and the path to the script being run
+const num1 = parseInt(process.argv[2])
+const num2 = parseInt(process.argv[3])
+console.log(sum(num1, num2));
