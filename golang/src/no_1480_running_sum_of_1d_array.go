784e3d5ac7a84f483f4658628ea5fbb020768222
hirotoe0112 <49823364+hirotoe0112@users.noreply.github.com>
2024-09-12 13:43:54 +0900

    1480/golang

---
+++ golang/src/no_1480_running_sum_of_1d_array.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	// Parse the command line arguments
+	flag.Parse()
+	args := flag.Args()
+	nums := make([]int, len(args))
+	for i, v := range args {
+		num, _ := strconv.Atoi(v)
+		nums[i] = num
+	}
+
+	fmt.Println(runningSum(nums))
+}
+
+func runningSum(nums []int) []int {
+	result := make([]int, len(nums))
+	for i, v := range nums {
+		if i == 0 {
+			result[i] = v
+		} else {
+			result[i] = v + result[i-1]
+		}
+	}
+	return result
+}
