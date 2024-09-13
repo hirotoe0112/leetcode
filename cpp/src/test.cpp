9c207d5e48efe3d6c9c51f3403e7f30974b8af43
hirotoe0112 <49823364+hirotoe0112@users.noreply.github.com>
2024-09-13 15:57:40 +0900

    cpp開発環境

---
+++ cpp/src/test.cpp
@@ -0,0 +1,9 @@
+// C++ Program Compilation Using GCC 
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello, Geeks!" << endl;
+      cout<< " This is a tutorial on Compiling a C++ program using GCC"<<endl;
+    return 0;
+}
