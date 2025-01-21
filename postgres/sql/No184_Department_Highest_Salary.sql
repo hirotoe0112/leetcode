2559276d782e3f1c9d4c78d1028c5287f0e9b495
hirotoe0112 <49823364+hirotoe0112@users.noreply.github.com>
2025-01-21 09:43:45 +0900

    184/postgres

---
+++ postgres/sql/No184_Department_Highest_Salary.sql
@@ -0,0 +1,55 @@
+--preparation
+CREATE TABLE Department (
+    id INT PRIMARY KEY,
+    name VARCHAR NOT NULL
+);
+
+CREATE TABLE Employee (
+    id INT PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    salary INT CHECK (salary > 0),
+    departmentId INT,
+    CONSTRAINT fk_department
+        FOREIGN KEY (departmentId)
+        REFERENCES Department (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+drop table department;
+drop table employee;
+
+INSERT INTO department
+VALUES
+    (1, 'IT'),
+    (2, 'Sales'),
+    (3, 'Marketing'),
+    (4, 'HR')
+;
+
+INSERT INTO employee VALUES
+(1, 'Alice', 70, 1)
+,(2, 'Bob', 60, 1)
+,(3, 'Tom', 80, 2)
+,(4, 'Cathy', 90, 3)
+,(5, 'David', 100, 3)
+,(6, 'Eva', 110, 4)
+,(7, 'Dena', 110, 4)
+,(8, 'Nina', 100, 4)
+
+select * from department;
+select * from employee;
+
+truncate table department;
+truncate table employee;
+
+--solution1
+with highest_salary as (
+    select MAX(salary) as salary, departmentid
+    from employee
+    group by departmentid
+)
+select d.name as "Department", e.name as "Employee", e.salary as "Salary"
+from highest_salary h
+inner join employee e on e.salary = h.salary and e.departmentid = h.departmentid
+inner join department d on d.id = e.departmentid;
